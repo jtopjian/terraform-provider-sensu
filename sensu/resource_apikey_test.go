803ca1873313b22bb248db3d686fe69a112911be
François Gouteroux <francois.gouteroux@gmail.com>
2021-07-27 10:00:09 +0200

    remove failing apikey update unit test

--- sensu/resource_apikey_test.go
+++ sensu/resource_apikey_test.go
@@ -10,7 +10,6 @@ import (
 
 func TestAccResourceAPIKey_basic(t *testing.T) {
 	username := acctest.RandomWithPrefix("sensu-user1")
-	username2 := acctest.RandomWithPrefix("sensu-user2")
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
@@ -21,13 +20,6 @@ func TestAccResourceAPIKey_basic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						"sensu_apikey.apikey_1", "username", username),
-				),
-			},
-			resource.TestStep{
-				Config: testAccResourceAPIKey_update(username2),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"sensu_apikey.apikey_1", "username", username2),
 				),
 			},
 		},
@@ -45,21 +37,6 @@ func testAccResourceAPIKey_basic(username string) string {
 
 		resource "sensu_apikey" "apikey_1" {
     	username = sensu_user.user_1.name
-  	}
-	`, username)
-}
-
-func testAccResourceAPIKey_update(username string) string {
-	return fmt.Sprintf(`
-		resource "sensu_user" "user_2" {
-			name = "%s"
-			password = "abcd1234"
-			groups = ["admin"]
-			disabled = false
-		}
-
-		resource "sensu_apikey" "apikey_1" {
-    	username = sensu_user.user_2.name
   	}
 	`, username)
 }
